ce41d150aa2e21ab0e3537221e180fe96ad98d9a
aparajit0511 <aparajit35@gmail.com>
2025-06-11 00:26:50 +0530

    adding service

--- src/main/java/com/example/BookStore/service/BookStoreServiceImpl.java
+++ src/main/java/com/example/BookStore/service/BookStoreServiceImpl.java
@@ -1,9 +1,41 @@
 package com.example.BookStore.service;
 
 
+import com.example.BookStore.dao.BookStoreRepository;
+import com.example.BookStore.entity.BookStore;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class BookStoreServiceImpl {
+public class BookStoreServiceImpl implements BookStoreService {
+
+    private BookStoreRepository bookStoreRepository;
+
+    @Autowired
+    public BookStoreServiceImpl(BookStoreRepository theBookStoreRepository){
+        bookStoreRepository = theBookStoreRepository;
+    }
+
+    public List<BookStore> findAll(){
+        return bookStoreRepository.findAll();
+    }
+
+    @Transactional
+    public BookStore save(BookStore bookStore){
+        return bookStoreRepository.save(bookStore);
+    }
+
+    @Transactional
+    public void deleteById(int theId){
+        bookStoreRepository.deleteById(theId);
+    }
+
+    public Optional<BookStore> findById(int theId){
+        return bookStoreRepository.findById(theId);
+    }
 
 }
